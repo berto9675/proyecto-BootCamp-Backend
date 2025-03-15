dd1e95006ae0cda6accd3befb3c942701a14fa15
berto9675 <albrtvd@.com>
2025-03-15 20:12:31 +0100

    nuevos cambios

--- ProyectoBackend/ProyectoBackend/src/main/java/com/BootCamp/Backend/ProyectoBackend/models/Category.java
+++ ProyectoBackend/ProyectoBackend/src/main/java/com/BootCamp/Backend/ProyectoBackend/models/Category.java
@@ -1,5 +1,26 @@
 package com.BootCamp.Backend.ProyectoBackend.models;
 
-public class Category {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "El nombre de la categoría no puede estar vacío")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
