87695546c52ba699b41049642582f050c2155bf1
berto9675 <albrtvd@.com>
2025-03-16 20:46:38 +0100

    Nuevos Cambios

---
+++ ProyectoBackend/ProyectoBackend/src/main/java/com/BootCamp/Backend/ProyectoBackend/controllers/Configurattion.java
@@ -0,0 +1,24 @@
+package com.BootCamp.Backend.ProyectoBackend.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.lang.NonNull;
+
+@Configuration
+public class Configurattion {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5170")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
---
+++ ProyectoBackend/ProyectoBackend/src/main/java/com/BootCamp/Backend/service/ResourceService.java
@@ -0,0 +1,5 @@
+package com.BootCamp.Backend.service;
+
+public class ResourceService {
+
+}
